c539127efd2952c87bad6c02b6c11f6245cb4480
Cristian Alfredo Manzo Parra <ca.manzo973@uniandes.edu.co>
2023-01-22 12:11:46 -0500

    chore. set MUI template

---
+++ web/src/assets/theme/theme.ts
@@ -0,0 +1,50 @@
+import { createTheme } from '@mui/material/styles'
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0576c1',
+    },
+    secondary: {
+      main: '#ff9501',
+    },
+  },
+  typography: {
+    h1: {
+      fontFamily: 'Montserrat',
+    },
+    h2: {
+      fontFamily: 'Montserrat',
+    },
+    h3: {
+      fontFamily: 'Montserrat',
+    },
+    h4: {
+      fontFamily: 'Montserrat',
+    },
+    h5: {
+      fontFamily: 'Montserrat',
+    },
+    h6: {
+      fontFamily: 'Montserrat',
+    },
+    subtitle1: {
+      fontFamily: 'Montserrat',
+    },
+    subtitle2: {
+      fontFamily: 'Montserrat',
+    },
+    button: {
+      fontFamily: 'Montserrat',
+    },
+    caption: {
+      fontFamily: 'Montserrat',
+    },
+    overline: {
+      fontFamily: 'Montserrat',
+    },
+  },
+  shape: {
+    borderRadius: 0,
+  },
+})
